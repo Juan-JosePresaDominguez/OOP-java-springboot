635777de52d718135e12fde33579cd189f7280ec
Juan-JosePresaDominguez <juan-jose.presa@dxc.com>
2024-02-13 21:01:23 +0100

    Sesión 12.02.2024 [AccountService]

--- 02.restful-implementation/AccountService/src/main/java/com/microcompany/accountsservice/controller/AccountServiceController.java
+++ 02.restful-implementation/AccountService/src/main/java/com/microcompany/accountsservice/controller/AccountServiceController.java
@@ -8,10 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /* Crear una aplicación AccountService que nos permita exponer una API para gestionar cuentas de usuario */
 @RestController
@@ -25,17 +24,42 @@ public class AccountServiceController {
     @Autowired
     private AccountRepository repo;
 
-    // Método POST (Crear Cuenta - SERVICIO)
+    // Métodos POST Equivalentes SERVICIO vs. REPOSITORIO
+    // NOTA: Dejar siempre uno de ellos comentado. De lo contrario se producirá el error 'to {POST [/accounts]}: There is already 'accountServiceController' bean method' y no se levantá el servidor.
+    // Método POST (Crear Cuenta 'create' - SERVICIO)
     @PostMapping("")
     public ResponseEntity<Account> create(@RequestBody Account newAccount) {
         return new ResponseEntity<>(service.create(newAccount), HttpStatus.CREATED);
     }
+    // Método POST (Crear Cuenta 'save' - REPOSITORIO)
+    /*@PostMapping("")
+    public ResponseEntity<Account> save(@RequestBody Account newAccount) {
+        logger.info("newAccount:" + newAccount);
+        return new ResponseEntity<>(repo.save(newAccount), HttpStatus.CREATED);
+    }*/
 
-    // Método POST (Crear Cuenta - REPOSITORIO)
-//    @PostMapping("")
-//    public ResponseEntity<Account> save(@RequestBody Account newAccount) {
-//        logger.info("newAccount:" + newAccount);
-//        return new ResponseEntity<>(repo.save(newAccount), HttpStatus.CREATED);
-//    }
+    // Método GET (Obtener Cuentas 'getAccounts' - SERVICIO)
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Account> getAccounts() {
+        return service.getAccounts();
+        //return repo.findAll();
+    }
+
+    // Método GET (Obtener Cuenta por ID 'getAccount' - SERVICIO)
+    @GetMapping("/{aid}")
+    public Account getAccount(@PathVariable("aid") Long id) {
+        return service.getAccount(id);
+    }
+
+    // Método GET (Obtener Cuenta por Owner 'getAccountByOwnerId' - SERVICIO)
+    @GetMapping("/{oid}")
+    public List<Account> getAccountByOwnerId(@PathVariable("oid") Long ownerId) {
+        return service.getAccountByOwnerId(ownerId);
+        //return repo.findByOwnerId(1L);
+    }
+
+
+
+
 
 }
